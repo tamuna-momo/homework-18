054ed3927d53d34ace9b59cf6bf4142c12775970
tamuna-momo <tamuna-momo@users.noreply.github.com>
2021-11-08 14:23:18 +0400

    add mouseenter and mouseleave in slider

--- app.js
+++ app.js
@@ -30,3 +30,89 @@
 // showTime();
 
 // 2.
+const slides = document.querySelectorAll('.slide-item');
+const slidesLength = slides.length;
+const nextButton = document.querySelector('#next');
+const prevButton = document.querySelector('#prev');
+const stopBtn = document.querySelector('#stop-sliding');
+const startBtn = document.querySelector('#start-sliding');
+let activeIndex = 0;
+
+function renderSlider() {
+  slides.forEach((element, index) => {
+    element.style.transform = `translateX(${100 * (index - activeIndex % slidesLength)}%)`;
+  })
+}
+
+renderSlider();
+
+function nextSlide() {
+  if(activeIndex === (slidesLength - 1)){
+    activeIndex = 0;
+  } else {
+    activeIndex = activeIndex + 1;
+  }
+
+  renderSlider();
+}
+
+function prevSlide() {
+  if(activeIndex === 0){
+    activeIndex = slidesLength - 1;
+  } else {
+    activeIndex = activeIndex - 1;
+  }
+
+  renderSlider();
+}
+
+nextButton.addEventListener('click', (e) => {
+  nextSlide();
+});
+prevButton.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', (e) => {
+  console.log(e.code);
+  if(e.code === 'ArrowRight'){
+    nextSlide();
+  }
+  if(e.code === 'ArrowLeft'){
+    prevSlide();
+  }
+});
+
+let intervalId = null;
+
+function startAutoSliding() {
+  if(!intervalId){
+    intervalId = setInterval(() => {
+      nextSlide();
+    }, 3000);
+  }
+}
+
+function stopAutoSliding() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+stopBtn.addEventListener('click', stopAutoSliding);
+startBtn.addEventListener('click', startAutoSliding);
+
+document.getElementById("slider").onmouseenter = function() {mouseEnter()};
+document.getElementById("slider").onmouseleave = function() {mouseLeave()};
+
+function mouseEnter() {
+    document.getElementById("slider")
+    clearInterval(intervalId);
+    intervalId = null;
+    
+}
+
+function mouseLeave() {
+    if(!intervalId){
+        document.getElementById("slider")
+        intervalId = setInterval(() => {
+          nextSlide();
+        }, 3000);
+      }
+}
